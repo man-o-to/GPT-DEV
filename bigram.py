c1887190f7134ede2b3a68c5337b7b1cfc2b4b5c
man-o-to <104087920+man-o-to@users.noreply.github.com>
2024-02-11 18:04:40 -0500

    Update bigram.py
    Added estimate_loss() to improve the loss prediction as every batch produced is "more or less lucky".
    
    This takes the mean from multiple batches.

--- bigram.py
+++ bigram.py
@@ -38,8 +38,22 @@ def get_batch(split):
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i:i+block_size] for i in ix])
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    x, y = x.to(device), y.to(device)
+    x, y = x.to(device), y.to(device) # load onto GPU for faster processing
     return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
 
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
@@ -80,13 +94,18 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 model = BigramLanguageModel(vocab_size)
-m = model.to(device)
+m = model.to(device) # load onto GPU for faster processing
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for iter in range(max_iters):
 
+    # periodically evaluate the loss on train and val sets
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    
     # sample a batch of data
     xb, yb = get_batch('train')
 
