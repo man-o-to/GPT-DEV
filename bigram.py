9ef1a18d0c92cba50ffbd85fa4c5e91f05e3e08c
man-o-to <104087920+man-o-to@users.noreply.github.com>
2024-02-11 18:28:00 -0500

    Refactor bigram.py
    vocab_size is no longer passed into functions (it was already defined as a global variable).

--- bigram.py
+++ bigram.py
@@ -58,7 +58,7 @@ def estimate_loss():
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
@@ -93,7 +93,7 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device) # load onto GPU for faster processing
 
 # create a PyTorch optimizer
